1a9c2ba767a9e48d7adad206ea1720334904d2ce
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2021-04-24 18:15:42 -0400

    feat: added issue creation message

--- src/action.js
+++ src/action.js
@@ -5,8 +5,16 @@ async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
-  const { issue } = github.context.payload;
-  console.log(issue);
+  const { context } = github;
+  const { issue } = context.payload;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: issue.number,
+    body: `Hello ${
+      issue?.user?.login ?? "Stranger"
+    }! Thank you for creating an issue!`,
+  });
 }
 
 run();
