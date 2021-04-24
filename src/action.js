a9fa488ace43c57a0e43b303805a243646e3cfc3
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2021-04-24 17:58:21 -0400

    feat: added octokit functionality

--- src/action.js
+++ src/action.js
@@ -1,5 +1,12 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
 async function run() {
-  console.log("Hello World");
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { issue } = github.context.payload;
+  console.log(issue);
 }
 
 run();
